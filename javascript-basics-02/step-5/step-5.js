137704935f8139d59debe53401622ef3abeaf353
Hasan1Aref <Hasan.aref.eng@gmail.com>
2021-03-15 12:16:54 +0200

    step-5

---
+++ javascript-basics-02/step-5/step-5.js
@@ -0,0 +1,18 @@
+var img = document.querySelector("img");
+img.addEventListener("mouseover", function () {
+    if (img.id == "image1") {
+        img.src = "images/image1_2.jpg";
+    } else if (img.id == "image2") {
+        img.src = "images/image2_2.jpg";
+    } else if (img.id == "images/image3_2.jpg") {
+        img.src = "images/image4_2.jpg";
+    } else if (img.id == "images/image5_2.jpg") {
+        img.src = "images/image5_2.jpg";
+    } else 
+    img.src="images / image1.jpg";
+    img.src="images / image2.jpg";
+    img.src="images / image3.jpg";
+    img.src="images / image4.jpg";
+    img.src="images / image5.jpg";
+
+});
